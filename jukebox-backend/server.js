4960f398c916355b918ccd1666bb800071e32f5f
Blitz17 <110586053+Blitz17@users.noreply.github.com>
2025-06-04 04:20:55 +1000

    3000 and 5000 working

--- jukebox-backend/server.js
+++ jukebox-backend/server.js
@@ -38,6 +38,7 @@ app.use(session({
   saveUninitialized: true
 }));
 
+app.use(express.static(path.join(__dirname, 'jukebox-frontend/public')));
 
 // MongoDB
 mongoose.connect(process.env.MONGO_URL, {
